26e92f871c320e906163b6cd18959bababb92210
Zack McKenna <zackmckenna@gmail.com>
2020-02-28 13:51:53 -0500

    Updates

---
+++ precache-manifest.966aa9c97b010c839dcc47fcfa18cc4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cec8cd1a035a1be648c14051b445bb6",
+    "url": "/github.io/UliftU/index.html"
+  },
+  {
+    "revision": "46eb47274aed87a7e995",
+    "url": "/github.io/UliftU/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c5c1c7b30acf208d3249",
+    "url": "/github.io/UliftU/static/css/main.e275826e.chunk.css"
+  },
+  {
+    "revision": "46eb47274aed87a7e995",
+    "url": "/github.io/UliftU/static/js/2.9a6930f8.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/github.io/UliftU/static/js/2.9a6930f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c1c7b30acf208d3249",
+    "url": "/github.io/UliftU/static/js/main.cbb7f13c.chunk.js"
+  },
+  {
+    "revision": "54224e7575089a4fc4da",
+    "url": "/github.io/UliftU/static/js/runtime-main.2e84cf07.js"
+  }
+]);
